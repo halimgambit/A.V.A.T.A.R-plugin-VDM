5576cd25903a0d064c58dadd141d197460b1ec97
halimgambit <70286759+halimgambit@users.noreply.github.com>
2026-06-29 19:39:23 +0200

    Update VDM.js

--- VDM/VDM.js
+++ VDM/VDM.js
@@ -6,53 +6,56 @@ export async function init () {
 }
 
 export async function action(data, callback) {
-
-    const L = await Avatar.lang.getPak('VDM', data.language);
-
     try {
+        const L = await Avatar.lang.getPak('VDM', data.language);
 
-         const tblActions = {
-            getVdm: () => getVdm(data.client, L)
+        const tblActions = {
+            getVdm: () => getVdm(data.client, L, callback)
         };
 
         info("VDM:", data.action.command, "from", data.client);
 
-       if (tblActions[data.action.command]) {
+        if (tblActions[data.action.command]) {
             await tblActions[data.action.command]();
+        } else {
+            callback();
         }
 
     } catch (err) {
         error("VDM Error:", err.message);
         if (data.client) Avatar.Speech.end(data.client);
+        callback();
     }
-
-    callback();
 };
 
+const getVdm = async (client, L, callback) => {
+    try {
+        const response = await fetch('http://www.viedemerde.fr/aleatoire');
+        
+        if (response.status !== 200) {
+            throw new Error(L.get(["speech.errorHttp"]));
+        }
+        
+        const html = await response.text();
+        const $ = cheerio.load(html);
 
-const getVdm = async (client, L) => {
-
-	try {
-
-	const response = await fetch('http://www.viedemerde.fr/aleatoire')
-	
-	if (response.status !== 200) {
-	    throw new Error(L.get("speech.errorHttp"));
-	}
-    
-	const html = await response.text();
-    const $ = cheerio.load(html);
-
-	const firstVdm = $('a.block.text-blue-500.dark\\:text-white.my-4').first().text();
+        const firstVdm = $('a.block.text-blue-500.dark\\:text-white.my-4').first().text().trim();
 
         if (!firstVdm) {
-            throw new Error(L.get("speech.errorEmpty"));
+            throw new Error(L.get(["speech.errorEmpty"]));
         }
 
+        const texte = L.get(["speech.vdm", firstVdm]);
 
-       Avatar.speak(L.get(["speech.vdm", firstVdm]), client, () => { Avatar.Speech.end(client) });
+        info(texte);
+
+        Avatar.speak(texte, client, () => { 
+            callback();
+        });
 
     } catch (err) {
-	Avatar.speak(`${L.get("speech.errorAccess")}, ${err.message}`, client, () => {Avatar.Speech.end(client)});
-	};
+        Avatar.speak(`${L.get(["speech.errorAccess"])}, ${err.message}`, client, () => {
+            callback();
+        });
+    }
 }
